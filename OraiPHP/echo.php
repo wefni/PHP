bbbdbff84293994e41600c8ff7483f1931628e60
Daweed <47455954+wefni@users.noreply.github.com>
2020-09-14 13:14:53 +0200

    OraiPHP
    Ezt kapd el ne a nathat!

---
+++ OraiPHP/echo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo "Egy", "Kettő". "Három", 4;
+      echo 5, "Hat",'hét';
+      echo "<br />";
+
+      echo "Egy", "kettő", "három", 4 ,"<br />", 5, "hat","hét";
+
+      echo "Egy", "kettő", "három", 4 ,"\n", 5, "hat","hét";
+
+     ?>
+
+  </body>
+</html>
